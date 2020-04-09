329eee39e91e3804678b95659aa58cb64e2674ab
Ciaran Chapman <ciaran.j.chapman@gamil.com>
2020-04-09 17:13:16 -0500

    Still working on generating run test file.

--- Combine-Blend/tests/test_combine_blend_files.py
+++ Combine-Blend/tests/test_combine_blend_files.py
@@ -33,148 +33,140 @@ def write_test_file(filename):
 
 def get_run_combinations():
 
+    run_config = {}
 
-    req_args = "-h --help -i --input_directory -I --input_filenames".split(" ")
-    opt_args1 = "-o  -O  -l  -L  -p  -p -P".split("  ")
-    opt_args2 = "--output_directory  --output_filename  --file_limit  --loop  --print_stats  --print_stats --stats_filename".split("  ")
+    req_args = "-h -i -I".split(" ")
+    long_req_args = "--help --input_directory --input_filenames".split(" ")
+    opt_args = "-o  -O  -l  -L  -p  -P".split("  ")
+    long_opt_args = "--output_directory  --output_filename  --file_limit  -loop  --print_stats  --stats_filename".split("  ")
 
+    args = []
+    long_args = []
+    #print(args)
+
+    help_vals = []
     in_dirs = "../test/blends/ | ../test/blends/my blends/ | ../does/not/exist/ | with/w!@rd//ch#r$/ |  ".split(" | ")
     in_files = "../tests/blends/Blahh.blend ../tests/blends/Cool.blend | ../tests/blends/Blahh.blend ../tests/blends/Cool.blend ../tests/blends/default.blend ../blends/my blends/Nuts N Bolts.blend ../blends/Treee_Scene.blend | does/not/exist.blend | /with/w!@rd//ch#r$/,blend | myblend |  ".split(" | ")
     limits = "-1 0 1 2 5 25 a * ?  ".split(" ")
+    loop = []
     out_dirs = "../ | ../test/blends/ | ../test/blends/my blends/ | ../doesnotexist/ | with/w!@rd//ch#r$/ |  ".split(" | ")
     out_files = "my out file | test.blend | out | cool!.blend | with-dashes.blend | with.dots.blend | with/slashes.blend | with~very*char'$,blend |  ".split(" | ")
-    s_files = "my stat file | test.txt | wow?.txt | with.dots.txt | with/slashes.txt | with~very*char'$,txt |  "
+    print_stats = []
+    s_files = "my stat file | test.txt | wow?.txt | with.dots.txt | with/slashes.txt | with~very*char'$,txt |  ".split(" | ")
 
-    combos = " "
-    for L in range(0, len(opt_args1)+1):
-        for sub1 in itertools.combinations(opt_args1, L):
-            for s1 in sub1:
-                combos += "%s " % s1
-            combos += " | "
+    a = [] + req_args + opt_args + long_req_args + long_opt_args
+    v = [] + [help_vals] + [in_dirs] + [in_files] + [out_dirs] + [out_files] + [limits] + [loop] + [print_stats] + [s_files]
+    v += [help_vals] + [in_dirs] + [in_files] + [out_dirs] + [out_files] + [limits] + [loop] + [print_stats] + [s_files]
 
-    for L in range(0, len(opt_args2)+1):
-        for sub1 in itertools.combinations(opt_args2, L):
-            for s1 in sub1:
-                combos += "%s " % s1
-            combos += " | "
+    #print(v)
 
-    print(combos)
+    run_config = dict(zip(a, v))
 
-    final_str = ""
-    for r in req_args:
-        for com in combos.split(" | "):
-            for c in com:
-                if r == "-i":
-                    for in_dir in in_dirs:
-                        i = "%s %s" % ("-i", in_dir)
-                        cc = ""
-                        for a in c.split(" "):
-                            if a == "-o" or a == "--output_directory":
-                                for out_d in out_dirs:
-                                    cc += "%s %s" % (c, out_d)
-                            if a == "-O" or a == "--output_filename":
-                                for out_f in out_files:
-                                    cc += "%s %s" % (c, out_f)
-                            if a == "-l" or a == "--file_limit":
-                                for fl in limits:
-                                    cc += "%s %s" % (c, fl)
-                            if a == "-P" or a == "--stats_filename":
-                                for sf in s_files:
-                                    cc += "%s %s" % (c, sf)
+    opt_args = "-o  -O  -l  -l -L  -p  -p -P".split("  ")
+    long_opt_args = "--output_directory  --output_filename  --file_limit  --file_limit --loop  --print_stats  --print_stats --stats_filename".split("  ")
 
-                        final_str += "python combine_blend_files.py %s %s\n" % (i, cc)
+    combos = []
 
-                if r == "--input_directory":
-                    for in_dir in in_dirs:
-                        i = "%s %s" % ("--input_directory", in_dir)
-                        cc = ""
-                        for a in c.split(" "):
-                            if a == "-o" or a == "--output_directory":
-                                for out_d in out_dirs:
-                                    cc += "%s %s" % (c, out_d)
-                            if a == "-O" or a == "--output_filename":
-                                for out_f in out_files:
-                                    cc += "%s %s" % (c, out_f)
-                            if a == "-l" or a == "--file_limit":
-                                for fl in limits:
-                                    cc += "%s %s" % (c, fl)
-                            if a == "-P" or a == "--stats_filename":
-                                for sf in s_files:
-                                    cc += "%s %s" % (c, sf)
+    for L in range(0, len(opt_args)+1):
+        for combo in itertools.combinations(opt_args, L):
+            combos.append(combo)
 
-                        final_str += "python combine_blend_files.py %s %s\n" % (i, cc)
+    for L in range(0, len(long_opt_args)+1):
+        for combo in itertools.combinations(long_opt_args, L):
+            combos.append(combo)
 
-                if r == "-I":
-                    for in_f in in_files:
-                        i = "%s %s" % ("-I", in_f)
-                        cc = ""
-                        for a in c.split(" "):
-                            if a == "-o" or a == "--output_directory":
-                                for out_d in out_dirs:
-                                    cc += "%s %s" % (c, out_d)
-                            if a == "-O" or a == "--output_filename":
-                                for out_f in out_files:
-                                    cc += "%s %s" % (c, out_f)
-                            if a == "-l" or a == "--file_limit":
-                                for fl in limits:
-                                    cc += "%s %s" % (c, fl)
-                            if a == "-P" or a == "--stats_filename":
-                                for sf in s_files:
-                                    cc += "%s %s" % (c, sf)
+    #print(combos)
 
-                        final_str += "python combine_blend_files.py %s %s\n" % (i, cc)
-                        
+    s = ""
+    for com in combos:
+        s += "%s|" % str(com)
+        for x in "( ) , '".split(" "):
+            s = s.replace(x, "")
+        #print(s)
 
-                if r == "--input_filenames":
-                    for in_dir in in_dirs:
-                        i = "%s %s" % ("--input_filenames", in_dir)
-                        cc = ""
-                        for a in c.split(" "):
-                            if a == "-o" or a == "--output_directory":
-                                for out_d in out_dirs:
-                                    cc += "%s %s" % (c, out_d)
-                            if a == "-O" or a == "--output_filename":
-                                for out_f in out_files:
-                                    cc += "%s %s" % (c, out_f)
-                            if a == "-l" or a == "--file_limit":
-                                for fl in limits:
-                                    cc += "%s %s" % (c, fl)
-                            if a == "-P" or a == "--stats_filename":
-                                for sf in s_files:
-                                    cc += "%s %s" % (c, sf)
+    x = s.split("|")
 
-                        final_str += "python combine_blend_files.py %s %s\n" % (i, cc)
+    a = []
+    for y in x:
+        z = y.split(" ")
+        z = list(dict.fromkeys(z))
 
-    return final_str
+        a += [z]
 
-    #return str(subset)
+    combos_array = []
+    
+    for com in a:
+        aa = "-i ".split(" ")  + com
+        aa.remove("")
+        combos_array.append(aa)
+    for com in a:
+        aa = "-I ".split(" ")  + com
+        aa.remove("")
+        combos_array.append(aa)
+    for com in a:
+        aa = "--input_directory ".split(" ")  + com
+        aa.remove("")
+        combos_array.append(aa)
+    for com in a:
+        aa = "--input_filenames ".split(" ")  + com
+        aa.remove("")
+        combos_array.append(aa)
 
-    # for r in r_args:
-    #     if r == "-h":
-    #         s += "python combine_blend_files.py -h\n"
+    #print(combos_array)
 
-    #     if r == "--help":
-    #         s += "python combine_blend_files.py --help\n"
-                
-    #     if r == "-i":
-    #         for i in in_dirs:
-    #             s += "python combine_blend_files.py -i %s\n" % i
+    run_config["combos"] = combos_array
 
-    #         for o in o_args:
-    #             if o == "-o":
-    #                 s += "python combine_blend_files.py -i %s" % o
+    #print(run_config)
 
-    #     if r == "--input_directory":
-    #         for i in in_dirs:
-    #             s += "python combine_blend_files.py --input_directory %s\n" % i
-    #     if r == "-I":
-    #         for i in in_files:
-    #             s += "python combine_blend_files.py -I %s\n" % i
-    #     if r == "--input_filenames":
-    #         for i in in_files:
-    #             s += "python combine_blend_files.py --input_filenames %s\n" % i
+    s = ""
+    prog = "python combine_blend_files.py"
+    s += "%s %s\n" % (prog, "-h")
+    s += "%s %s\n" % (prog, "--help")
+    for key, val in run_config.items():
+        if key == "combos":
+            for combo in run_config.get(key):
+                s_combo = str(combo)
+                #print(s_combo)
+                for com in combo:
 
-    #return s
+                    com_key = com
+                    com_vals = run_config.get(com_key)
+                    #print("com_key = %s" % com_key)
+                    #print("com_val = %s" % com_vals)
+                    s += "%s %s" % (prog, com_key)
+
+                    i = 0
+                    k = ""
+                    if not com_vals == None:
+                        for v in com_vals:
+                            if i < len(com_vals):
+                                if not com_key == k:
+                                    if com_key == "-i":
+                                        s += "\n%s %s " % (prog, com_key)
+                                        #print(v)
+                                        s += "%s" % v
+                                    else:
+                                        s += " %s " % com_key
+                                        s += "%s" % v
+                                else:
+                                    s += "\n%s %s " % (prog, com_key)
+                                    #print(v)
+                                    s += "%s" % v
+
+                            if i == len(com_vals) - 1:
+                                s += "\n" if not "-i" in com_key else ""
+
+                            k = com_key
+                            i += 1
+
+                    s += "\n"
+                            #print("%s %s %s" % (prog, com_key, v))
+
+                #print(s_combo)
+                    
+
+    print(s)
+    return s
 
 
 def main():
